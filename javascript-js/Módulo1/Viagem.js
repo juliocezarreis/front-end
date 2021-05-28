5735c2f1ee7b93d9b87ed41268a161c3a0a6ce7f
juliocezarreis <julioreis1000.pro@gmail.com>
2021-05-28 09:25:17 -0300

    Adding upgrade

--- javascript-js/Módulo1/Viagem.js
+++
@@ -1,8 +0,0 @@
-const destinos = new Array(`Salvador`, `São Paulo`, `Picos`, `Floriano`, `Cancun`)
-console.log(`Imprimindo destinos:\n${destinos}\n`)
-
-destinos.push(`Fernando de Noronha`)
-console.log(`Adicionando destino:\n${destinos}\n`)
-
-destinos.splice(0, 2)
-console.log(`Removendo destinos:\n${destinos}\n`)
